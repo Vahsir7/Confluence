3c0c65b82584931afd450aebdfbbb169d8b8fe39
Rishav <rishavbairagya@gmail.com>
2024-08-09 01:00:56 +0530

    Add css files

--- confluence-flask/application/__init__.py
+++ confluence-flask/application/__init__.py
@@ -1,26 +1,40 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-
+from flask_login import LoginManager
 
 db = SQLAlchemy()
 migrate = Migrate()
+login_manager = LoginManager()
 
 def create_app():
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.sqlite3'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['UPLOAD_FOLDER'] = 'application/static/uploads'
-    
+    app.config['SECRET_KEY'] = 'your_secret_key'  # Ensure you have a secret key for session management
+
     db.init_app(app)
     migrate.init_app(app, db)
+    login_manager.init_app(app)
 
+    # Import the models and routes to ensure they are registered
     with app.app_context():
-        from .models import Influencers, Sponsors, Campaigns, InfluencerAccounts, Applications
+        from .models import Influencers, Sponsors, InfluencerAccounts
         db.create_all()
-    
-    # Import the routes to ensure they are registered
+
+    # Register the blueprints
     from .controller import main_bp
     app.register_blueprint(main_bp)
     
+    login_manager.login_view = 'main.login'
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        # Adjust this to suit how you differentiate between InfluencerAccounts and Sponsors
+        user = InfluencerAccounts.query.get(int(user_id))
+        if user is None:
+            user = Sponsors.query.get(int(user_id))
+        return user
+
     return app
